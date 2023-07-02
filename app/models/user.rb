7c9673a84432303dfe29b37521604116590d4b53
keiakamom <128572313+keiakamom@users.noreply.github.com>
2023-07-02 09:57:37 +0900

    ユーザー管理機能のテスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,22 +4,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
  
-  validates :nickname, presence: true
-  validates :email, uniqueness: true
-  validates :birthday, presence: true
-                
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'パスワードは半角英数字の両方を含めて入力してください' 
-                
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角で入力してください' } do
-   validates :first_name
-   validates :last_name
-  end
+         validates :nickname, presence: true
+         validates :email, uniqueness: true
+         validates :birthday, presence: true
+                       
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         validates_format_of :password, with: PASSWORD_REGEX
+                       
+         with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do 
+          validates :first_name
+          validates :last_name
+         end
+                     
+         with_options presence: true, format: { with: /\A[ァ-ヶ一ー]+\z/} do 
+          validates :first_name_kana
+          validates :last_name_kana
+         end
               
-  with_options presence: true, format: { with: /\A[ァ-ヶ一ー]+\z/, message: '全角カタカナで入力してください' } do
-   validates :first_name_kana
-   validates :last_name_kana
-  end
-       
- 
-end
+      end

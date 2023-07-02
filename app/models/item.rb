5d2ac5a804a59e669355bffa31c71f2308730eca
keiakamom <128572313+keiakamom@users.noreply.github.com>
2023-07-02 21:38:55 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,14 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   belongs_to :user
   has_one    :order
   has_one_attached :image
+
+  belongs_to :category
+  belongs_to :prefecture 
+  belongs_to :status 
+  belongs_to :shipping_fee 
+  belongs_to :shipping_date
 
 end

e62cf56b4b7e6f7120a78c54e64072c91c457bfb
keiakamom <128572313+keiakamom@users.noreply.github.com>
2023-07-02 21:52:08 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,4 +11,26 @@ class Item < ApplicationRecord
   belongs_to :shipping_fee 
   belongs_to :shipping_date
 
+  with_options presence: true do
+    validates :user_id
+    validates :image
+    validates :name
+    validates :info 
+    validates :category_id
+    validates :status_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :shipping_date_id
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 } 
+  end
+
+  with_options numericality: { other_than: 1 , message: "can't be blank" } do
+    validates :category_id
+    validates :status_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :shipping_date_id
+  end
+
+
 end

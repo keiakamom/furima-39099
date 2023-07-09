e4eea425889e219cccf16b3eb20c7a38171b301f
keiakamom <128572313+keiakamom@users.noreply.github.com>
2023-07-09 21:32:34 +0900

    商品削除機能の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
-  before_action :move_to_index, only: [:edit, :update]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -35,8 +35,6 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    return unless @item.user_id == current_user.id
-
     @item.destroy
     redirect_to root_path
   end

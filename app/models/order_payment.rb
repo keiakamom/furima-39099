3390312ce23020df82ad6614d1e033eef1778cf2
keiakamom <128572313+keiakamom@users.noreply.github.com>
2023-07-12 21:36:34 +0900

    Formオブジェクトの作成

---
+++ app/models/order_payment.rb
@@ -0,0 +1,20 @@
+class OrderPayment
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postcode, :prefecture_id, :city, :adresses, :building, :phone_number
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" } 
+    validates :city
+    validates :adresses
+    validates :building
+    validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "is invalid. Include hyphen(-)"}
+  end
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Payment.create(postcode: postcode, prefecture_id: prefecture_id, city: city, adresses: adresses, building: building, phone_number: phone_number, order_id: order.id)
+  end
+end

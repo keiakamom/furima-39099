04ab88bcb0fda57dbf0422591bc5daa5de4e37ea
keiakamom <128572313+keiakamom@users.noreply.github.com>
2023-07-20 21:20:07 +0900

    フォームの実装、フォームの情報保存

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,25 @@
 class OrdersController < ApplicationController
 
   def index
+    @item = Item.find(params[:item_id])
     @order_payment = OrderPayment.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @order_payment = OrderPayment.new(order_params)
+    if @order_payment.valid?
+      @order_payment.save
+      redirect_to root_path
+    else  
+      render :index
+    end 
+  end 
+
+  private
+
+  def order_params
+    params.require(:order_payment).permit(:postcode, :prefecture_id, :city, :adresses, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
-   belings_to :user
+   belongs_to :user
    belongs_to :item
    has_one :payment
 end
